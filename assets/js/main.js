8bbe4bc612372f61305b79f95f9aa5e5652a59ca
rodrigoviturino <viturino.souza1@gmail.com>
2019-07-20 18:09:06 -0300

    Deixando dados dinamicos na view, buscando do products.json

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,40 @@
 
-// const url = 'products.json';
-// axios(url).then(resp => {
-//     // Data já retorna o json convertido em objeto
-//     const itens = resp.data.reduce(
-//         (html,estado) => html + `<li>${estado.cart.item[0].name}</li>`, ''
-//     )
-
-//     document.body.insertAdjacentHTML('beforeend', `<ul>${itens}</ul>`)
-// });
-
-axios.get('http://127.0.0.1:5500/challenge/assets/js/products.json')
+axios.get('http://127.0.0.1:5500/assets/js/products.json')
   .then(function(response){
       // console.log(dados);
       let dados = response.data.cart.item;
     
       for (let i = 0; i < dados.length; i++) {
-
-        let t = document.createElement('li');
-        let tt = document.querySelector('#tela').appendChild(t).innerHTML = dados[i].name;
+        // let li = document.createElement('li');
+        // let tela = document.querySelector('#tela').appendChild(li).innerHTML = dados[i].name;
         
-    }
-    // console.log(response.status); // ex.: 200
+        let listaProdutos = document.createElement('div');
+
+        let produto = 
+        `
+          <div class="produto">
+            <img src="${dados[i].image}" alt="">
+            <p>${dados[i].name.substring(0,30)} ...</p>
+          </div>
+        `;
+        listaProdutos.innerHTML = produto;
+      
+        document.querySelector('.produtos').appendChild(listaProdutos);
+        
+      }
+      let valorTotal = [];
+      dados.forEach(dado => {
+        valorTotal.push(dado.bestPrice);
+        // console.log(valorTotal);        
+      });
+
+      
+      valorTotal = valorTotal.reduce( (a,b)  => a + b,0);
+      let valorTotalFormatado = valorTotal.toString().split('',-1);        
+      valorTotalFormatado.splice(2,0,'.');
+      valorTotalFormatado.splice(6,0,',');
+      console.log(`R$ ${valorTotalFormatado.join('')}`);
+      
   })
   .catch(function(error){
       if(error){
