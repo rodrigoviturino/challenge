b9dd3499dc25a36b2083bae29cba1fcb459c22c5
rodrigoviturino <viturino.souza1@gmail.com>
2019-07-20 20:47:05 -0300

    Adicionando estilo responsivo

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,31 +12,41 @@ axios.get('http://127.0.0.1:5500/assets/js/products.json')
 
         let produto = 
         `
-          <div class="produto">
-            <img src="${dados[i].image}" alt="">
-            <p>${dados[i].name.substring(0,30)} ...</p>
+          <div class="box__cart__produtos__produto">
+            <div>
+              <img src="${dados[i].image}" alt="">
+            </div>
+            <div class="box__cart__produtos__produto__info">
+              <p>${dados[i].name.substring(0,30)} ...</p>
+            <div class="box__cart__produtos__produto__info__desc">
+              <span class="qtd">Qtd: ${dados[i].quantity}</span>
+              <span class="price">${dados[i].bestPriceFormated}</span>
+            </div>
+            </div>
           </div>
         `;
         listaProdutos.innerHTML = produto;
       
-        document.querySelector('.produtos').appendChild(listaProdutos);
+        document.querySelector('.js_produtos').appendChild(listaProdutos);
         
       }
+
       let valorTotal = [];
       dados.forEach(dado => {
         valorTotal.push(dado.bestPrice);
         // console.log(valorTotal);        
       });
+      
+        valorTotal = valorTotal.reduce( (a,b)  => a + b,0);
+        let valorTotalFormatado = valorTotal.toString().split('',-1);        
+        valorTotalFormatado.splice(2,0,'.');
+        valorTotalFormatado.splice(6,0,',');
 
-      
-      valorTotal = valorTotal.reduce( (a,b)  => a + b,0);
-      let valorTotalFormatado = valorTotal.toString().split('',-1);        
-      valorTotalFormatado.splice(2,0,'.');
-      valorTotalFormatado.splice(6,0,',');
-      console.log(`R$ ${valorTotalFormatado.join('')}`);
-      
-  })
-  .catch(function(error){
+       let vlTl = valorTotalFormatado.join('');
+
+       document.querySelector('#vlTotal').innerHTML = ` R$ ${vlTl}`;
+
+  }).catch(function(error){
       if(error){
           console.log(error)
       }
